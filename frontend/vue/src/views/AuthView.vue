508d33e302f8e3cde96bd2733cad75194005e7d3
Kirillkriv <kirill.krivoy@icloud.com>
2025-05-05 19:31:01 +0500

    feat - AuthView QoL improvements

--- frontend/vue/src/views/AuthView.vue
+++ frontend/vue/src/views/AuthView.vue
@@ -1,15 +1,38 @@
 <script setup lang="ts">
 import AuthForm from "@/components/AuthForm.vue";
+import {useUserStore} from "@/stores/user.ts";
+
+import {
+  NCard
+} from 'naive-ui'
+
+const userStore = useUserStore()
 </script>
 
 <template>
   <div class="container flex justify-center items-center">
-    <AuthForm/>
+    <n-card
+      v-if="userStore.isAuthorized"
+      :title="() => `Welcome back, ${userStore.username}!`"
+      class="n-card"
+    >
+      You are already authorized, so please <RouterLink to="/" class="ref">proceed forward</RouterLink>.
+    </n-card>
+    <AuthForm v-else/>
   </div>
 </template>
 
 <style scoped>
+  .n-card {
+    text-align: center;
+    min-width: 40%;
+    width: fit-content;
+  }
   .container {
     height: 100%;
+  }
+  .ref {
+    color: rgb(99, 226, 183);
+    border-bottom: rgb(99, 226, 183) 0.07em dashed;
   }
 </style>
